674cd2cc87f6c5b8bfdd32eb39b9f3f11aa1deaf
ChrisCodes54 <christangarife95@gmail.com>
2021-12-04 08:24:29 -0500

    tested out the delete feature and tested the wildcard route

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -6,9 +6,6 @@ let db = require('../db/db.json');
 
 // initially getting all notes in the db.json
 router.get("/notes", (req,res)=>{
-    // res.sendFile(path.join(__dirname, "../db/db.json"))
-    // const rawNotes = fs.readFileSync("./db/db.json");
-    // const parseNotes = JSON.parse(rawNotes);
     res.json(db);
 })
 
@@ -16,7 +13,7 @@ router.post("/notes", (req,res) => {
     let id = db.length + 1;
     req.body.id = id;
     db.push(req.body);
-    // console.log(req)
+    
     fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(db))
     res.send(`${req.method} note added!`)
 })
@@ -26,9 +23,8 @@ router.delete("/notes/:id", (req,res) => {
     let remainingNotes = db.filter((note) => {
         return (note.id != req.params.id) 
     })
-
     fs.writeFileSync(path.join(__dirname,"../db/db.json"),JSON.stringify(remainingNotes))
-    res.send(`Note Deleted!!!!!!!!!!!!!!!!`)    
+    res.send(`Note Deleted!`)    
 })
 
 module.exports = router;
