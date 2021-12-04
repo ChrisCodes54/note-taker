f82f7881ce50ee8e3187b35fce0c3f455a742ce8
ChrisCodes54 <christangarife95@gmail.com>
2021-12-03 19:31:01 -0500

    created the delete and post methods for the API routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,21 +1,34 @@
 const router = require("express").Router();
 const path = require('path');
-const fs = require("fs")
+const fs = require("fs");
+let db = require('../db/db.json');
 
 
 // initially getting all notes in the db.json
 router.get("/notes", (req,res)=>{
-    res.sendFile(path.join(__dirname, "../db/db.json"))
+    // res.sendFile(path.join(__dirname, "../db/db.json"))
+    // const rawNotes = fs.readFileSync("./db/db.json");
+    // const parseNotes = JSON.parse(rawNotes);
+    res.json(db);
 })
 
-// write a note (post) to the db.json
+router.post("/notes", (req,res) => {
+    let id = db.length + 1;
+    req.body.id = id;
+    db.push(req.body);
+    // console.log(req)
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(db))
+    res.send(`${req.method} note added!`)
+})
 
 
+router.delete("/notes/:id", (req,res) => {
+    let remainingNotes = db.filter((note) => {
+        return (note.id != req.params.id) 
+    })
 
-
-
-// delete a note by its id from the db.json
-
-
+    fs.writeFileSync(path.join(__dirname,"../db/db.json"),JSON.stringify(remainingNotes))
+    res.send(`Note Deleted!!!!!!!!!!!!!!!!`)    
+})
 
 module.exports = router;
